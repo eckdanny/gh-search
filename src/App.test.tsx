813254ec0dc637e115cec64e45a1d4bb1d509e46
Danny Eck <eck.danny@gmail.com>
2019-05-13 16:54:03 -0700

    add basic UI elems, add a11y tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,10 +3,16 @@ import { render } from 'react-testing-library'
 import App from './App'
 
 describe('App', () => {
-  it('includes a link to React docs', () => {
-    const { getByText } = render(<App />)
-    expect(getByText('Learn React').getAttribute('href')).toMatch(
-      /\/\/reactjs.org/
-    )
+  it('includes a [role="banner"] region', () => {
+    const { getByRole } = render(<App />)
+    expect(getByRole('banner')).toBeInTheDocument()
+  })
+  it('includes a [role="main"] region', () => {
+    const { getByRole } = render(<App />)
+    expect(getByRole('main')).toBeInTheDocument()
+  })
+  it('has a level-one heading', () => {
+    const { container } = render(<App />)
+    expect(container.getElementsByTagName('h1').length).toBe(1)
   })
 })
